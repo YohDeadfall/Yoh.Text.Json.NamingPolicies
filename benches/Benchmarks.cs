9bdf4e1e3a17576d31ddf4b876394beefda24af6
Yoh Deadfall <yoh.deadfall@hotmail.com>
2021-03-21 00:19:38 +0100

    Simplified benchmarks project

--- benches/Benchmarks.cs
+++ benches/Benchmarks.cs
@@ -1,10 +1,13 @@
-﻿using BenchmarkDotNet;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace Yoh.Text.Json.NamingPolicies.Benchmarks
 {
     public class Benchmarks
     {
+        public static void Main(string[] args) =>
+            BenchmarkRunner.Run<Benchmarks>();
+
         [Params("XMLHttpRequest")]
         public string Name { get; set; }
 
