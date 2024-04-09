b7071a6897e9b4aaa04b97084464c3f630daa139
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-04-09 23:52:49 +0300

    Added a base class for naming policies

---
+++ src/JsonNamingPolicyBase.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Buffers;
+using System.Globalization;
+using System.Text.Json;
+
+namespace Yoh.Text.Json.NamingPolicies
+{
+    internal abstract class JsonNamingPolicyBase : JsonNamingPolicy
+    {
+        public sealed override string ConvertName(string name)
+        {
+            // Rented buffer 20% longer that the input.
+            var rentedBufferLength = (12 * name.Length) / 10;
+            var rentedBuffer = rentedBufferLength > JsonConstants.StackallocCharThreshold
+                ? ArrayPool<char>.Shared.Rent(rentedBufferLength)
+                : null;
+
+            var resultUsedLength = 0;
+            Span<char> result = rentedBuffer is null
+                ? stackalloc char[JsonConstants.StackallocCharThreshold]
+                : rentedBuffer;
+
+            void ExpandBuffer(ref Span<char> result)
+            {
+                var newBuffer = ArrayPool<char>.Shared.Rent(result.Length * 2);
+
+                result.CopyTo(newBuffer);
+
+                if (rentedBuffer is not null)
+                {
+                    result.Slice(0, resultUsedLength).Clear();
+                    ArrayPool<char>.Shared.Return(rentedBuffer);
+                }
+
+                rentedBuffer = newBuffer;
+                result = rentedBuffer;
+            }
+
+            void WriteWord(ReadOnlySpan<char> word, ref Span<char> result)
+            {
+                if (word.IsEmpty)
+                    return;
+
+                while (true)
+                {
+                    var written = TryWriteWord(resultUsedLength == 0, word, result.Slice(resultUsedLength));
+                    if (written > 0)
+                    {
+                        resultUsedLength += written;
+                        return;
+                    }
+
+                    ExpandBuffer(ref result);
+                }
+            }
+
+            int first = 0;
+            var chars = name.AsSpan();
+            var previousCategory = CharCategory.Boundary;
+            for (int index = 0; index < chars.Length; index++)
+            {
+                var current = chars[index];
+                var currentCategoryUnicode = char.GetUnicodeCategory(current);
+                if (currentCategoryUnicode == UnicodeCategory.SpaceSeparator ||
+                    currentCategoryUnicode >= UnicodeCategory.ConnectorPunctuation &&
+                    currentCategoryUnicode <= UnicodeCategory.OtherPunctuation)
+                {
+                    WriteWord(chars.Slice(first, index - first), ref result);
+
+                    previousCategory = CharCategory.Boundary;
+                    first = index + 1;
+
+                    continue;
+                }
+
+                if (index + 1 < chars.Length)
+                {
+                    var next = chars[index + 1];
+                    var currentCategory = currentCategoryUnicode switch
+                    {
+                        UnicodeCategory.LowercaseLetter => CharCategory.Lowercase,
+                        UnicodeCategory.UppercaseLetter => CharCategory.Uppercase,
+                        _ => previousCategory
+                    };
+
+                    if (currentCategory == CharCategory.Lowercase && char.IsUpper(next) ||
+                        next == '_')
+                    {
+                        WriteWord(chars.Slice(first, index - first + 1), ref result);
+
+                        previousCategory = CharCategory.Boundary;
+                        first = index + 1;
+
+                        continue;
+                    }
+
+                    if (previousCategory == CharCategory.Uppercase &&
+                        currentCategoryUnicode == UnicodeCategory.UppercaseLetter &&
+                        char.IsLower(next))
+                    {
+                        WriteWord(chars.Slice(first, index - first), ref result);
+
+                        previousCategory = CharCategory.Boundary;
+                        first = index;
+
+                        continue;
+                    }
+
+                    previousCategory = currentCategory;
+                }
+            }
+
+            WriteWord(chars.Slice(first), ref result);
+
+            name = result.Slice(0, resultUsedLength).ToString();
+
+            if (rentedBuffer is not null)
+            {
+                result.Slice(0, resultUsedLength).Clear();
+                ArrayPool<char>.Shared.Return(rentedBuffer);
+            }
+
+            return name;
+        }
+
+        protected abstract int TryWriteWord(bool first, ReadOnlySpan<char> word, Span<char> destination);
+
+        private enum CharCategory
+        {
+            Boundary,
+            Lowercase,
+            Uppercase,
+        }
+    }
+}
--- src/JsonSeparatorNamingPolicy.cs
+++ src/JsonSeparatorNamingPolicy.cs
@@ -5,7 +5,7 @@ using System.Text.Json;
 
 namespace Yoh.Text.Json.NamingPolicies
 {
-    internal abstract class JsonSeparatorNamingPolicy : JsonNamingPolicy
+    internal abstract class JsonSeparatorNamingPolicy : JsonNamingPolicyBase
     {
         private readonly bool _lowercase;
         private readonly char _separator;
@@ -16,145 +16,26 @@ namespace Yoh.Text.Json.NamingPolicies
             _separator = separator;
         }
 
-        public override string ConvertName(string name)
+        protected override int TryWriteWord(bool first, ReadOnlySpan<char> word, Span<char> destination)
         {
-            // Rented buffer 20% longer that the input.
-            var rentedBufferLength = (12 * name.Length) / 10;
-            var rentedBuffer = rentedBufferLength > JsonConstants.StackallocCharThreshold
-                ? ArrayPool<char>.Shared.Rent(rentedBufferLength)
-                : null;
-
-            var resultUsedLength = 0;
-            Span<char> result = rentedBuffer is null
-                ? stackalloc char[JsonConstants.StackallocCharThreshold]
-                : rentedBuffer;
-
-            void ExpandBuffer(ref Span<char> result)
+            var offset = first ? 0 : 1;
+            if (offset < destination.Length)
             {
-                var newBuffer = ArrayPool<char>.Shared.Rent(result.Length * 2);
-
-                result.CopyTo(newBuffer);
-
-                if (rentedBuffer is not null)
+                if (!first)
                 {
-                    result.Slice(0, resultUsedLength).Clear();
-                    ArrayPool<char>.Shared.Return(rentedBuffer);
+                    destination[0] = _separator;
                 }
 
-                rentedBuffer = newBuffer;
-                result = rentedBuffer;
+                destination = destination.Slice(offset);
+
+                var written = _lowercase
+                    ? word.ToLowerInvariant(destination)
+                    : word.ToUpperInvariant(destination);
+
+                return written + offset;
             }
 
-            void WriteWord(ReadOnlySpan<char> word, ref Span<char> result)
-            {
-                if (word.IsEmpty)
-                    return;
-
-                int written;
-                while (true)
-                {
-                    var destinationOffset = resultUsedLength != 0
-                        ? resultUsedLength + 1
-                        : resultUsedLength;
-
-                    if (destinationOffset < result.Length)
-                    {
-                        var destination = result.Slice(destinationOffset);
-
-                        written = _lowercase
-                            ? word.ToLowerInvariant(destination)
-                            : word.ToUpperInvariant(destination);
-
-                        if (written > 0)
-                            break;
-                    }
-
-                    ExpandBuffer(ref result);
-                }
-
-                if (resultUsedLength != 0)
-                {
-                    result[resultUsedLength] = _separator;
-                    resultUsedLength += 1;
-                }
-
-                resultUsedLength += written;
-            }
-
-            int first = 0;
-            var chars = name.AsSpan();
-            var previousCategory = CharCategory.Boundary;
-            for (int index = 0; index < chars.Length; index++)
-            {
-                var current = chars[index];
-                var currentCategoryUnicode = char.GetUnicodeCategory(current);
-                if (currentCategoryUnicode == UnicodeCategory.SpaceSeparator ||
-                    currentCategoryUnicode >= UnicodeCategory.ConnectorPunctuation &&
-                    currentCategoryUnicode <= UnicodeCategory.OtherPunctuation)
-                {
-                    WriteWord(chars.Slice(first, index - first), ref result);
-
-                    previousCategory = CharCategory.Boundary;
-                    first = index + 1;
-
-                    continue;
-                }
-
-                if (index + 1 < chars.Length)
-                {
-                    var next = chars[index + 1];
-                    var currentCategory = currentCategoryUnicode switch
-                    {
-                        UnicodeCategory.LowercaseLetter => CharCategory.Lowercase,
-                        UnicodeCategory.UppercaseLetter => CharCategory.Uppercase,
-                        _ => previousCategory
-                    };
-
-                    if (currentCategory == CharCategory.Lowercase && char.IsUpper(next) ||
-                        next == '_')
-                    {
-                        WriteWord(chars.Slice(first, index - first + 1), ref result);
-
-                        previousCategory = CharCategory.Boundary;
-                        first = index + 1;
-
-                        continue;
-                    }
-
-                    if (previousCategory == CharCategory.Uppercase &&
-                        currentCategoryUnicode == UnicodeCategory.UppercaseLetter &&
-                        char.IsLower(next))
-                    {
-                        WriteWord(chars.Slice(first, index - first), ref result);
-
-                        previousCategory = CharCategory.Boundary;
-                        first = index;
-
-                        continue;
-                    }
-
-                    previousCategory = currentCategory;
-                }
-            }
-
-            WriteWord(chars.Slice(first), ref result);
-
-            name = result.Slice(0, resultUsedLength).ToString();
-
-            if (rentedBuffer is not null)
-            {
-                result.Slice(0, resultUsedLength).Clear();
-                ArrayPool<char>.Shared.Return(rentedBuffer);
-            }
-
-            return name;
-        }
-
-        private enum CharCategory
-        {
-            Boundary,
-            Lowercase,
-            Uppercase,
+            return 0;
         }
     }
 }
